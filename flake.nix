dab5db738c85f4c02664bad79fdecbe987caced6
Providence Salumu <Providence.Salumu@smunix.com>
2021-01-30 09:44:21 -0500

    hih, initial commit

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Simply Typed Lambda Calculus";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils/master";
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    with flake-utils.lib;
+    eachSystem [ "x86_64-linux" ] (system:
+      with (import nixpkgs { inherit system; }); {
+        packages = flattenTree {
+          hih =
+            with haskell.lib;
+            with haskellPackages;
+            overrideCabal (callCabal2nix "hih" ./. {}) (_: {});
+        };
+      });
+}
---
+++ lib/Lexer/Lexer.hs
@@ -0,0 +1,2 @@
+-- |
+module Lexer.Lexer where
